9e1eb9e434820b9196771874ab462f647850d886
wsadzanie2 <pawlito2007@gmail.com>
2023-08-10 20:50:46 +0200

    Made the Text Boxes a bit better

--- main.py
+++ main.py
@@ -303,8 +303,8 @@ class TextInput:
         self.text_rect = self.text_surface.get_rect()
         self.text_rect.midleft = self.rect.midleft
         self.rect.width = self.text_rect.width
-        if len(self.text) <= 1:
-            self.rect.width += 10
+        if self.rect.width <= 10:
+            self.rect.width = 10
 
     def draw(self, surface):
         self.update_values()
