f66e55eba5c1b6e90688d92b4e495ecb669ee596
wsadzanie2 <pawlito2007@gmail.com>
2023-08-12 01:16:05 +0200

    Expanded the pygame-blocks "plugin"

--- pygame-blocks.py
+++ pygame-blocks.py
@@ -1,4 +1,9 @@
 BLOCKS = [
     {'color': (255, 255, 0), 'text': "PYGAME", 'command': 'import pygame', 'arguments': 0, 'tab_increase': 0},
-    {'color': (255, 255, 0), 'text': "screen", 'command': 'pygame.display.set_mode', 'arguments': 2, 'tab_increase':0}
+    {'color': (255, 255, 0), 'text': "screen", 'command': 'pygame.display.set_mode', 'arguments': 2, 'tab_increase':0},
+    {'color': (255, 255, 0), 'text': "set_caption", 'command': 'pygame.display.set_caption', 'arguments': 1, 'tab_increase':0},
+    {'color': (255, 255, 0), 'text': "quit", 'command': 'pygame.quit', 'arguments': 1, 'tab_increase':0},
+    {'color': (255, 255, 0), 'text': "clock", 'command': 'pygame.time.Clock', 'arguments': 1, 'tab_increase':0},
+    {'color': (255, 255, 0), 'text': "delay", 'command': 'pygame.time.delay', 'arguments': 1, 'tab_increase':0},
+    {'color': (255, 255, 0), 'text': "event loop", 'command': 'for event in pygame.event.get()', 'arguments': 0, 'tab_increase':1}
     ]
