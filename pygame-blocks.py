29b02bd630fb4ebb2519b9a3c281da3309d0de33
wsadzanie2 <pawlito2007@gmail.com>
2023-08-12 01:03:36 +0200

    Added basic support for pygame with pygame-blocks.py and made it easier to load other blocks

--- pygame-blocks.py
+++ pygame-blocks.py
@@ -1,3 +1,4 @@
 BLOCKS = [
-    {'color': (255, 255, 0), 'text': "PYGAME", 'command': 'import pygame', 'arguments': 1, 'tab_increase': 0}
+    {'color': (255, 255, 0), 'text': "PYGAME", 'command': 'import pygame', 'arguments': 0, 'tab_increase': 0},
+    {'color': (255, 255, 0), 'text': "screen", 'command': 'pygame.display.set_mode', 'arguments': 2, 'tab_increase':0}
     ]
