8da1009eee198ba234601a40434293c22778dfac
wsadzanie2 <pawlito2007@gmail.com>
2023-09-01 15:40:13 +0200

    Fixed sys-blocks.py

--- sys-blocks.py
+++ sys-blocks.py
@@ -1,5 +1,4 @@
 BLOCKS = [
     {'color': (206, 147, 112), 'text': "SYS", 'command': 'import sys', 'arguments': 0, 'tab_increase': 0},
-    {'color': (206, 147, 112), 'text': "EXIT", 'command': 'sys.exit', 'arguments': 1, 'tab_increase':0},
-    {'color': (206, 147, 112), 'text': "Get current working directory", 'command': 'os.getcwd', 'arguments': 1, 'tab_increase': 0},
+    {'color': (206, 147, 112), 'text': "EXIT", 'command': 'sys.exit', 'arguments': 1, 'tab_increase': 0},
     ]
